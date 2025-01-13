5bd0451c89ccce9cde4f2d38ffe96708e318bb1d
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2025-01-13 16:43:11 -0500

    fix folded text length
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- src/main/kotlin/com/razomy/notation/jetbrains/intellij/ide/plugin/RnFoldingBuilder.kt
+++ src/main/kotlin/com/razomy/notation/jetbrains/intellij/ide/plugin/RnFoldingBuilder.kt
@@ -7,10 +7,22 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import kotlin.math.min
 
-fun countTrailingSpaces(str: String): Int {
+fun countTrailingSpacesWithNewLines(str: String): Int {
     var count = 0
     for (i in str.lastIndex downTo 0) {
         if (str[i] == ' ' || str[i] == '\n') {
+            count++
+        } else {
+            break
+        }
+    }
+    return count
+}
+
+fun countTrailingSpaces(str: String): Int {
+    var count = 0
+    for (i in str.lastIndex downTo 0) {
+        if (str[i] == ' ') {
             count++
         } else {
             break
@@ -24,21 +36,28 @@ class RnFoldingBuilder : FoldingBuilder {
     override fun buildFoldRegions(root: ASTNode, document: Document): Array<FoldingDescriptor> {
         val descriptors: MutableList<FoldingDescriptor> = ArrayList()
 
-        fun add(nodeFrom: ASTNode, nodeTo: ASTNode) {
-            val text = nodeFrom.text.substring(0, min(120, nodeFrom.text.length))
-            val start = nodeFrom.startOffset
-            val trailingSpaces = countTrailingSpaces(nodeTo.text);
-            val end = nodeTo.textRange.endOffset - trailingSpaces;
-            if (end <= start) {
-                return
+        fun add(propertyNode: ASTNode) {
+            val trailingSpaces = countTrailingSpacesWithNewLines(propertyNode.text);
+            val removed_last_line_spaces = propertyNode.textRange.endOffset - trailingSpaces;
+            val range = TextRange(propertyNode.textRange.startOffset, removed_last_line_spaces)
+
+            var prevtext = propertyNode.treePrev?.text;
+            if (prevtext == null) {
+                prevtext = ""
             }
-            val range = TextRange(start, end)
+
+            val leadingSpaces = countTrailingSpaces(prevtext);
+            val from_text_in_range_120_with_ellipces = propertyNode.text.substring(
+                0,
+                min(120 - 3 - leadingSpaces, propertyNode.text.length)
+            ) + "..."
+            val placeholderText = from_text_in_range_120_with_ellipces
 
             val descriptor = FoldingDescriptor(
-                    nodeFrom,
-                    range,
-                    null,
-                    text
+                propertyNode,
+                range,
+                null,
+                placeholderText
             )
 
             descriptors.add(descriptor)
@@ -46,7 +65,11 @@ class RnFoldingBuilder : FoldingBuilder {
 
         fun processNode(node: ASTNode) {
             if (node.elementType == RnTypes.PROPERTY) {
-                add(node, node.lastChildNode)
+                if (node.textRange.length == 0) {
+                    return
+                }
+
+                add(node)
             }
 
             for (child in node.getChildren(null)) {
